28202472674a46c67d1dc3a4ce0544acf91b1b06
kritika sharma <kritikasharma2281@gmail.com>
2025-05-04 21:38:47 -0400

    first commit

---
+++ test/ecs/variable.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  default = "second-ecs-tf-cluster"
+
+}
+
+variable "project_name" {
+  default = "sample-app"
+}
+
+variable "cpu" {
+  default = 256
+}
+
+variable "memory" {
+  default = 512
+}
+
+variable "container_port" {
+  default = 80
+}
---
+++ test/s3/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+module "s3_bucket" {
+  source = "https://github.com/kritika618/terraform_new_modules.git//.modules/aws/s3"
+
+  bucket = "my-s3-bucket-1001"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
