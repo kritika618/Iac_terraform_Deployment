0b06ea12931bd5abb1016c3c443eeaeaac2d8f9c
kritika sharma <kritikasharma2281@gmail.com>
2025-05-05 00:20:27 -0400

    add test

--- test/eks/main.tf
+++ test/eks/main.tf
@@ -1,21 +1,20 @@
 provider "aws" {
-  region = var.region
+  region = "ap-south-1"  # Directly hardcoded if desired
 }
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
+  cluster_name    = "terraform-eks-cluster"
   cluster_version = "1.29"
-  subnets         = var.private_subnets
-  vpc_id          = var.vpc_id
+  subnets         = ["subnet-0a433e5614138a125", "subnet-0cc71f61342a9a205"]
+  vpc_id          = "vpc-0a19349c1563bf053"
 
   eks_managed_node_groups = {
     default = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
-
-      instance_types = ["t3.medium"]
+      instance_types   = ["t3.medium"]
     }
   }
 }
--- test/eks/variable.tf
+++ test/eks/variable.tf
@@ -1,23 +1,19 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  region         = "ap-south-1"
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
+  description = "EKS cluster name"
   type        = string
-  cluster_name   = "terraform-eks-cluster"
 }
 
 variable "vpc_id" {
-  description = "VPC ID where EKS will be deployed"
+  description = "VPC ID"
   type        = string
-  vpc_id         = "vpc-0a19349c1563bf053"
 }
 
 variable "private_subnets" {
-  description = "List of private subnet IDs for EKS nodes"
+  description = "Private subnets"
   type        = list(string)
-  private_subnets = ["subnet-0a433e5614138a125", "subnet-0cc71f61342a9a205"]
 }
