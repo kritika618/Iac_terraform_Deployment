f2533e2f88f8e3aea0d38ea8547b751769a70eb4
kritika sharma <kritikasharma2281@gmail.com>
2025-05-05 01:42:48 -0400

    add test

--- test/eks/main.tf
+++ test/eks/main.tf
@@ -14,7 +14,7 @@ module "eks" {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
-      instance_types   = ["t3.medium"]
+      instance_types   = ["t2.medium"]
     }
   }
 }
