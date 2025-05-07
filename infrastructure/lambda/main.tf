61958bb6bf3a18a939a0dc7e87ff18d7c84fb87c
kritika618 <kritikasharma2281@gmail.com>
2025-05-07 14:13:49 -0400

    Update main.tf

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -3,7 +3,7 @@ module "lambda_function" {
   version = "~> 7.4"
   
   function_name  = "lambda-github"
-  create_package = false 
+  create_package = false  
   package_type   = "Image" 
   image_uri      = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/lambda-github:v1.0.0"
 }
