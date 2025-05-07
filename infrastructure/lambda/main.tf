47b1ac726bade51b60d6aba3d2a50c4f4f2779ba
kritika sharma <kritikasharma2281@gmail.com>
2025-05-07 11:23:59 -0400

    add yaml

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "lambda_exec_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
+  role       = "arn:aws:iam::273354669111:role/Tf_Lambda_function"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -27,6 +27,6 @@ resource "aws_lambda_function" "docker_lambda" {
   function_name = "hello-docker-lambda"
   package_type  = "Image"
   image_uri     = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/lambda-github:v1.0.0"
-  role          = aws_iam_role.lambda_exec_role.arn
+  role          = "arn:aws:iam::273354669111:role/Tf_Lambda_function"
   timeout       = 10
 }
