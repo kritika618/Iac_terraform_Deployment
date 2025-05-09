d3a15f5741b81ae840cb4f4d57cf2a7519c48575
kritika618 <kritikasharma2281@gmail.com>
2025-05-08 22:06:04 -0400

    Update main.tf

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -2,8 +2,8 @@ module "lambda_function" {
   source          = "terraform-aws-modules/lambda/aws"
   version         = "~> 7.4"
 
-  function_name   = var.function_name
+  function_name   = "hello-lambda"
   create_package  = false 
   package_type    = "Image" 
-  image_uri       = var.image_uri
+  image_uri       = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/lambda-github:v1.0.0"
 }
