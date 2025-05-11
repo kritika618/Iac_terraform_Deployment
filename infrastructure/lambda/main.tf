00d9d5a8b732b97de50663ad6779faa4a7e7ba68
kritika sharma <kritikasharma2281@gmail.com>
2025-05-11 15:46:45 -0400

    Trigger lambda workflow

---
+++ infrastructure/lambda/main.tf
@@ -0,0 +1,9 @@
+module "lambda_function" {
+  source          = "terraform-aws-modules/lambda/aws"
+  version         = "~> 7.4"
+
+  function_name   = "hello-lambda-github"
+  create_package  = false
+  package_type    = "Image"
+  image_uri       = "273354669111.dkr.ecr.ap-south-1.amazonaws.com/lambda-github:v1.0.0"
+}
